730ce2aadde95efcf32fcac2e56f7564306cf6cc
ot07 <ot07neoki@gmail.com>
2022-12-09 18:54:13 +0900

    ✨ add TransactionItem.tsx and TransactionList.tsx

---
+++ src/components/TransactionItem.tsx
@@ -0,0 +1,31 @@
+type Props = {
+  name: string;
+  description: string;
+  amount: string;
+  date: string;
+  icon: () => JSX.Element;
+};
+
+export const TransactionItem = ({
+  name,
+  description,
+  amount,
+  date,
+  icon: Icon,
+}: Props) => {
+  return (
+    <li className="mx-1 flex items-start rounded-lg p-2 hover:bg-orange-50">
+      <div className="flex h-10 w-10 shrink-0 items-center justify-center text-white">
+        <Icon aria-hidden="true" />
+      </div>
+      <div className="ml-4 flex-1">
+        <p className="mb-1 text-sm font-semibold text-slate-700">{name}</p>
+        <p className="text-xs">{description}</p>
+      </div>
+      <div className="ml-4 text-right">
+        <p className="mb-1 text-sm font-semibold text-red-500">$ {amount}</p>
+        <p className="text-xs">{date}</p>
+      </div>
+    </li>
+  );
+};
--- src/hooks/useMutateExpenses.ts
+++ src/hooks/useMutateExpenses.ts
@@ -1,6 +1,19 @@
 import { trpc } from "../utils/trpc";
 
 export const useMutateExpenses = () => {
-  const createExpenseMutation = trpc.expenses.createExpense.useMutation();
+  const utils = trpc.useContext();
+
+  const createExpenseMutation = trpc.expenses.createExpense.useMutation({
+    onSuccess: (res) => {
+      const previousExpenses = utils.expenses.getExpenses.getData();
+      if (previousExpenses) {
+        utils.expenses.getExpenses.setData(undefined, [
+          ...previousExpenses,
+          res,
+        ]);
+      }
+    },
+  });
+
   return { createExpenseMutation };
 };
