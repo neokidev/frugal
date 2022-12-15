9c7cae9b2a1b1991903a1f83891d8b00042438d6
ot07 <ot07neoki@gmail.com>
2022-12-15 21:20:40 +0900

    💄 implement dark mode on login page

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -17,14 +17,14 @@ const loginButtonItems = [
 export const Auth = () => {
   return (
     <div className="flex h-screen w-full items-center justify-center">
-      <div className="flex w-[28rem] flex-col items-center justify-center overflow-hidden break-words rounded-2xl border border-black/5 bg-white px-10 py-12 shadow-md shadow-black/5">
+      <div className="flex w-[28rem] flex-col items-center justify-center overflow-hidden break-words rounded-2xl border border-black/5 bg-white px-10 py-12 shadow-md shadow-black/5 dark:border-white/5 dark:bg-gray-900">
         <h1 className="mb-8 text-3xl font-semibold">Frugal</h1>
 
         <div className="w-full [&>*:not(:last-child)]:mb-4">
           {loginButtonItems.map(({ label, icon, onClick }) => (
             <button
               key={label}
-              className="flex w-full transform items-center justify-center space-x-2 rounded-lg border px-4 py-2 text-center text-sm text-gray-600 transition-colors duration-300 hover:bg-gray-100"
+              className="flex w-full transform items-center justify-center space-x-2 rounded-lg border bg-white px-4 py-2 text-center text-sm text-gray-600 transition-colors duration-300 hover:bg-gray-100"
               onClick={onClick}
             >
               {icon}
